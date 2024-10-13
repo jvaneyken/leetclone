1b624c9a4e427c24953a4c3b91698c57227fd8fe
Josh van Eyken <josh.vaneyken@gmail.com>
2024-10-13 14:28:17 -0700

    Conditionally display contents of Topbar if problem page or not

---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type TimerProps = {};
+
+const Timer: React.FC<TimerProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default Timer;
---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,11 @@
+import Topbar from "@/components/Topbar/Topbar";
+import React from "react";
+
+// type ProblemPageProps = {};
+
+const ProblemPage: React.FC = () => {
+  return <div>
+    <Topbar problemPage />
+  </div>;
+};
+export default ProblemPage;
