99d50f4c14d935ce17f5c1502bf9763f0c94d47b
Josh van Eyken <josh.vaneyken@gmail.com>
2024-10-14 13:50:19 -0700

    Test cases UI

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Split from "react-split";
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
+import Playground from "./Playground/Playground";
 // type WorkspaceProps = {};
 
 const Workspace: React.FC = () => {
   return (
     <Split className="split" minSize={0}>
       <ProblemDescription />
-      <div>Code Editor</div>
+      <Playground />
     </Split>
   );
 };
