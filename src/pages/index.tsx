6fd5706e2250f72586fc04abbd88bb2af5155145
Josh van Eyken <josh.vaneyken@gmail.com>
2024-10-16 14:55:54 -0700

    Add problems to DB

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
+// import { firestore } from "@/firebase/firebase";
+// import { doc, setDoc } from "firebase/firestore";
+// import { useState } from "react";
 
 export default function Home() {
+  // const [inputs, setInputs] = useState({
+  //   id: "",
+  //   title: "",
+  //   difficulty: "",
+  //   category: "",
+  //   videoId: "",
+  //   link: "",
+  //   order: 0,
+  //   likes: 0,
+  //   dislikes: 0,
+  // });
+
+  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setInputs({
+  //     ...inputs,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   // convert inputs.order to integer
+  //   const newProblem = {
+  //     ...inputs,
+  //     order: Number(inputs.order),
+  //   }
+  //   await setDoc(doc(firestore, "problems", inputs.id), newProblem);
+  //   alert("Saved to DB");
+  // }
+
   return (
     <>
       <main className="bg-dark-layer-2 min-h-screen">
@@ -9,6 +42,53 @@ export default function Home() {
         <h1 className="text-2xl text-center text-gray-700 dark:text-gray-400 font-medium uppercase mt-10 mb-5">
           &ldquo; QUALITY OVER QUANTITY &rdquo; 👇
         </h1>
+
+        {/* temp form */}
+        {/* <form className="p-6 flex flex-col max-w-sm gap-3" onSubmit={handleSubmit}>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="problem id"
+            name="id"
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="title"
+            name="title"
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="difficulty"
+            name="difficulty"
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="category"
+            name="category"
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="order"
+            name="order"
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="videoId?"
+            name="videoId"
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            placeholder="link?"
+            name="link"
+          />
+          <button className="bg-white">Save to DB</button>
+        </form> */}
 
         <div className="relative overflow-x-auto mx-auto px-6 pb-10">
           <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
