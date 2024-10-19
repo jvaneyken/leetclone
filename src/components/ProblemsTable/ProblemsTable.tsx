35acdaa4b04fab51ac8bdab8f6deb467d964b3ae
Josh van Eyken <josh.vaneyken@gmail.com>
2024-10-18 18:26:42 -0700

    fetch problems from DB

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { problems } from "@/mockProblems/problems";
 import { BsCheckCircle } from "react-icons/bs";
 import Link from "next/link";
 import { AiFillYoutube } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
 import YouTube from "react-youtube";
+import {
+  collection,
+  orderBy,
+  query,
+  getDocs,
+} from "firebase/firestore";
+import { firestore } from "@/firebase/firebase";
+import { DBProblem } from "@/utils/types/problem";
+// import { useAuthState } from "react-firebase-hooks/auth";
 
-// type ProblemsTableProps = {
+type ProblemsTableProps = {
+  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-// };
-
-const ProblemsTable: React.FC = () => {
+const ProblemsTable: React.FC<ProblemsTableProps> = ({
+  setLoadingProblems,
+}) => {
   const [youbePlayer, setYoutubePlayer] = useState({
     isOpen: false,
     videoId: "",
   });
+
+  const problems = useGetProblems(setLoadingProblems);
 
   const closeModal = () => {
     setYoutubePlayer({ isOpen: false, videoId: "" });
@@ -33,17 +45,17 @@ const ProblemsTable: React.FC = () => {
   return (
     <>
       <tbody className="text-white">
-        {problems.map((doc, idx) => {
+        {problems.map((problem, idx) => {
           const difficultyColor =
-            doc.difficulty === "Easy"
+            problem.difficulty === "Easy"
               ? "text-dark-green-s"
-              : doc.difficulty === "Medium"
+              : problem.difficulty === "Medium"
               ? "text-dark-yellow"
               : "text-dark-pink";
           return (
             <tr
               className={`${idx % 2 === 1 ? "bg-dark-layer-1" : ""}`}
-              key={doc.id}
+              key={problem.id}
             >
               <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
                 <BsCheckCircle fontSize="18" width="18" />
@@ -51,24 +63,24 @@ const ProblemsTable: React.FC = () => {
               <td className="px-6 py-4">
                 <Link
                   className="hover:text-blue-600 cursor-pointer"
-                  href={`/problems/${doc.id}`}
+                  href={`/problems/${problem.id}`}
                 >
-                  {doc.title}
+                  {problem.title}
                 </Link>
               </td>
               <td className={`px-6 py-4 ${difficultyColor}`}>
-                {doc.difficulty}
+                {problem.difficulty}
               </td>
-              <td className="px-6 py-4">{doc.category}</td>
+              <td className="px-6 py-4">{problem.category}</td>
               <td className="px-6 py-4">
-                {doc.videoId ? (
+                {problem.videoId ? (
                   <AiFillYoutube
                     fontSize={"28"}
                     className="cursor-pointer hover:text-red-600"
                     onClick={() =>
                       setYoutubePlayer({
                         isOpen: true,
-                        videoId: doc.videoId as string,
+                        videoId: problem.videoId as string,
                       })
                     }
                   />
@@ -108,3 +120,30 @@ const ProblemsTable: React.FC = () => {
   );
 };
 export default ProblemsTable;
+
+function useGetProblems(
+  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  const [problems, setProblems] = useState<DBProblem[]>([]);
+
+  useEffect(() => {
+    const getProblems = async () => {
+      // fetching data logic
+      setLoadingProblems(true);
+      const q = query(
+        collection(firestore, "problems"),
+        orderBy("order", "asc")
+      );
+      const querySnapshot = await getDocs(q);
+      const tmp: DBProblem[] = [];
+      querySnapshot.forEach((doc) => {
+        tmp.push({ id: doc.id, ...doc.data() } as DBProblem);
+      });
+      setProblems(tmp);
+      setLoadingProblems(false);
+    };
+
+    getProblems();
+  }, [setLoadingProblems]);
+  return problems;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
 // import { firestore } from "@/firebase/firebase";
 // import { doc, setDoc } from "firebase/firestore";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [loadingProblems, setLoadingProblems] = useState(true);
   // const [inputs, setInputs] = useState({
   //   id: "",
   //   title: "",
@@ -91,8 +92,16 @@ export default function Home() {
         </form> */}
 
         <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+          {loadingProblems && (
+            <div className="max-w-[1200px] mx-auto sm:w-7/12 w-full animate-pulse">
+              {[...Array(10)].map((_, idx) => (
+                <LoadingSkeleton key={idx} />
+              ))}
+            </div>
+          )}
           <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
-            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
+            {!loadingProblems && (
+              <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
               <tr>
                 <th scope="col" className="px-1 py-3 w-0 font-medium">
                   Status
@@ -112,10 +121,24 @@ export default function Home() {
                 </th>
               </tr>
             </thead>
-            <ProblemsTable />
+            )}
+            <ProblemsTable setLoadingProblems={setLoadingProblems} />
           </table>
         </div>
       </main>
     </>
   );
 }
+
+const LoadingSkeleton = () => {
+  return (
+    <div className="flex items-center space-x-12 mt-4 px-6">
+      <div className="w-6 h-6 shrink-0 rounded-full bg-dark-layer-1"></div>
+      <div className="h-8 sm:w-52  w-32 rounded-full bg-dark-layer-1"></div>
+      <div className="h-8 sm:w-52  w-32 rounded-full bg-dark-layer-1"></div>
+      <div className="h-8 sm:w-52 w-32 rounded-full bg-dark-layer-1"></div>
+      <div className="h-8 sm:w-52 w-32 rounded-full bg-dark-layer-1"></div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
